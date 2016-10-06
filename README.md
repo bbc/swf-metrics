da5ce1d420234d550a554fcab275afce0bee6f8c
Thomas Parisot <thomas.parisot@bbc.co.uk>
2016-10-06 15:52:34 +0100

    Display errors

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # swf-metrics
-Introspective metrics and breakdowns of pending and finished AWS Simple Workflow executions.
+
+> Introspective metrics and breakdowns of pending and finished AWS Simple Workflow executions.
+
+# Use
+
+## Failed workflows
+
+```
+swf-metrics failed
+```
+
+![](screenshot-errors.png)
--- test/consolidate-history.js
+++ test/consolidate-history.js
@@ -3,12 +3,17 @@ const { consolidate } = require('../lib/workflow/history.js');
 const fixtures = require('./fixtures/history.json');
 
 test('workflow/history#consolidate', t => {
-  t.plan(5);
+  t.plan(3);
 
   const execution = {
     workflowId: 'foo',
     runId: 'bar'
   };
+
+  const EXPECTED_EXECUTION = Object.assign({}, execution, {
+    startedAt: '2016-10-04T11:46:51.301Z',
+    completedAt: '',
+  });
 
   const EXPECTED_ACTIVITY = {
     activityType: {
@@ -20,14 +25,13 @@ test('workflow/history#consolidate', t => {
     startedAt: '2016-10-04T11:53:41.710Z',
     completedAt: '2016-10-04T11:53:47.605Z',
     identity: 'efa9db31b6b5:1',
-    activityId: 'ce4ced6e-649d-4f9a-85e4-2cbaa71f8528'
+    activityId: 'ce4ced6e-649d-4f9a-85e4-2cbaa71f8528',
+    stdout: '{"properties":["http://schema.org/url","http://prototyping.bbc.co.uk/ns#html"]}'
   };
 
   const workflow = consolidate(execution, fixtures);
 
-  t.equal(workflow.startedAt, '2016-10-04T11:46:51.301Z');
-  t.equal(workflow.completedAt, '');
   t.equal(workflow.activities.length, 16);
-  t.deepEqual(workflow.execution, execution);
+  t.deepEqual(workflow.execution, EXPECTED_EXECUTION);
   t.deepEqual(workflow.activities[0], EXPECTED_ACTIVITY);
 });
