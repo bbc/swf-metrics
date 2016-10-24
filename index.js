a9128a3ce83bd947eaa7e98700460caf48c53fe9
Thomas Parisot <thomas.parisot@bbc.co.uk>
2016-10-24 20:14:58 +0100

    Expose workflow history API for now

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-const Batch = require('batch');
-const { consolidate } = require('./lib/workflow/history.js');
+const { getWorkflowsHistory } = require('./lib/workflow/history.js');
 const { getOpenWorkflows, getFailedWorkflows, getCompletedWorkflows } = require('./lib/workflow/get.js');
 const { countOpenWorkflows } = require('./lib/workflow/count.js');
-
-// so far, this is a good number for large workflows but maybe it should be adapted based on the number of history to retrieve
-const DEFAULT_CONCURRENCY = 15;
 
 function getPendingWorkflowsActivities(client, options) {
   return Promise.resolve(client)
@@ -29,45 +25,10 @@ function countPendingWorkflowsActivities(client, options) {
     .then(() => countOpenWorkflows(client, options));
 }
 
-function getWorkflowsHistory (client, data, options) {
-  return new Promise((resolve, reject) => {
-    const { domain } = options;
-    const b = new Batch();
-    b.concurrency(DEFAULT_CONCURRENCY);
-
-    data.forEach((execution, index) => {
-      b.push(next => {
-        const params = Object.assign({}, { execution }, { domain });
-        client.getWorkflowExecutionHistory(params, (err, history) => {
-          if (err) {
-            return next(err);
-          }
-
-          try {
-            data[index] = consolidate(execution, history.events);
-            next();
-          }
-          catch(err) {
-            return next(err);
-          }
-        });
-      });
-    });
-
-    b.end(err => {
-      if (err) {
-        return reject(err);
-      }
-
-      resolve(data);
-    });
-  });
-}
-
-
 module.exports = {
   getPendingWorkflowsActivities,
   getCompletedWorkflowsActivities,
   getFailedWorkflowsActivities,
   countPendingWorkflowsActivities,
+  getWorkflowsHistory,
 };
